6a759827a0ad90f519a319bf0662aba4156ef425
sher6en <sher6en@DESKTOP-VK4RSI6.localdomain>
2020-02-09 08:52:18 +0200

    Fixed second question of dry part.

--- Dry Part/dryPartCode.cpp
+++ Dry Part/dryPartCode.cpp
@@ -8,21 +8,27 @@ using std::vector;
 bool isSorted(vector<int> v);
 template<typename InputIterator, typename Condition>
 int checkCondition(InputIterator first, InputIterator last, Condition condition);
+int place_in_sorted_array(vector<int> v, const int& num);
 
 class bigger{
+	vector<int> v;
 public:
-    bigger() = default;
+    bigger(vector<int> vector) : v(vector) {}
     ~bigger() = default;
     bigger(const bigger&) = default;
     bigger& operator=(const bigger&) = default;
     bool operator()(int a, int b) const {
-        return (a<b);
+		cout << a << " " << b << " Is a<b " << (a<b) << " Is index a < index b " <<  (place_in_sorted_array(v,a) < place_in_sorted_array(v,b)) <<endl;
+        if (a < b && place_in_sorted_array(v,a) < place_in_sorted_array(v,b)){
+			return 1;
+		}
+		return 0;
     }
 };
 
 //int main(){
 //    vector<int> v;
-//    for (int i = 1; i<=4; i++){
+//	for (int i = 1; i<=4; i++){
 //        v.push_back(i);
 //    }
 //    cout << isSorted(v) << endl;
@@ -49,9 +55,31 @@ int checkCondition(InputIterator first, InputIterator last, Condition condition)
 bool isSorted(vector<int> v){
     vector<int>::iterator first = v.begin();
     vector<int>::iterator last = v.end();
-    bigger condition;
+    bigger condition(v);
     int size = v.size();
     //amount of numbers that meet the condition of array is sorted.
     int amount_if_sorted = ((size) * (size-1))/2;
     return (checkCondition(first, last, condition) == amount_if_sorted);
 }
+
+int place_in_sorted_array(vector<int> v, const int& num) {
+	vector<int>::iterator iterator = v.begin();
+	int counter = 0;
+	for (; iterator!=v.end(); iterator++) {
+		if (*iterator > num){
+			counter++;
+		}
+	}
+	return v.size() - counter;
+}
+	
+
+
+
+
+
+
+
+
+
+
